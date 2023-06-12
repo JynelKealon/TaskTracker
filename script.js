dc8eb9514b40f55070502b187dbc127e0dc046fd
Jynel Kealon <jkealon13@r2happrentice.org>
2023-06-12 12:57:07 -0400

    new changes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Imports the express modules and sets it to variables 
 const express = require('express');
 const app = express();
+app.use(express.json());
 const tasks = require('./tasks.json');
+const cors = require('cors');
+
+app.use(cors());
 
 //Grabbing the information that is held within the 'employee' JSON file
 app.get('/tasks', (req, res) => {
@@ -20,7 +24,22 @@ app.get('/tasks/:taskId', (req, res) => {
   }
 });
 
+app.post('/tasks', (req, res) => {
+    let data = req.body;
+    console.log(data)
+    res.send('Data Received: ' + JSON.stringify(data));
+});
+
 // Shows what port the server is running on 
 app.listen(3000, () => {
   console.log('Server is running on port 3000.');
 });
+
+fetch('/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ /* your data here */ })
+  })
+  
